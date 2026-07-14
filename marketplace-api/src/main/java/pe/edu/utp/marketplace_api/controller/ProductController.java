66e175593cf479ee43127d4e1ccd40c7046a4498
GeoJJ <114630662+GeoJJ@users.noreply.github.com>
2026-07-14 01:49:57 -0500

    Update ProductController.java

--- marketplace-api/src/main/java/pe/edu/utp/marketplace_api/controller/ProductController.java
+++ marketplace-api/src/main/java/pe/edu/utp/marketplace_api/controller/ProductController.java
@@ -9,19 +9,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "*") // ¡Clave! Permite que tu Frontend en HTML se conecte sin bloqueos de seguridad (CORS)
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    // Método para LEER los productos (Lo usará el catálogo)
     @GetMapping
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    // Método para CREAR un producto (Lo usará el panel de Administrador)
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return productRepository.save(product);
